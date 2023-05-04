e3f010e85cc21e98ec65e7a05fefd731c3fdab18
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2023-05-04 12:09:33 +0100

    Dictionary adds stripped version of word with newline just in case google dictionary has trailing whitespace

--- dictionary.rb
+++ dictionary.rb
@@ -3,9 +3,10 @@
   dic_file = File.new('edited_word_list.txt', 'w')
   old_dic = File.open('google-10000-no-swears.txt', 'r')
   old_dic.each do |word|
-    dic_file.puts word if word.strip.length.between?(5, 12)
+    dic_file.puts "#{word.strip}\n" if word.strip.length.between?(5, 12)
     # allows for the newline character at the end of each line, so the words chosen
     # have between 5 and 12 characters as required
+    # even if original dictionary had trailing whitespace, the edited list will not
   end
   old_dic.close
   dic_file.close
