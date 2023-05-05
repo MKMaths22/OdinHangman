59bab90cbccdf5e1547a85d5c1adb7c263b3d92a
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2023-05-05 08:38:28 +0100

    Write random word method for start of the game

--- hangman.rb
+++ hangman.rb
@@ -4,10 +4,13 @@ class Game
     end
 
     def find_random_word
-        dictionary = File.open('google-10000-no-swears.txt', 'r')
-
+        dictionary = File.open('edited_word_list.txt', 'r')
+        dictionary.readlines.sample.chomp
     end
 
 
 
 end
+
+this_game = Game.new
+puts this_game.find_random_word
